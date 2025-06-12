15a2cd508a90b4a840c6b4e7677cf91709bc3de7
Henry Agu <aguhenrychuks@gmail.com>
2025-06-12 13:51:56 +0100

    feat: shadcn ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
-import { Link } from "@tanstack/react-router"
+import { Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
-  return <div className="flex items-center h-screen justify-center w-full">
-    <Link to="/resumeForm" className="bg-black text-white text-base px-5 py-2.5 rounded-sm transition-all duration-200 ease-in-out hover:opacity-75">Get started</Link>
-  </div>
+  return (
+    <div className="flex items-center h-screen justify-center w-full">
+      <Link
+        to="/resumeForm"
+        className="bg-black text-white text-base px-6 py-3.5 rounded-sm transition-all duration-200 ease-in-out hover:opacity-75"
+      >
+        Get started
+      </Link>
+    </div>
+  );
 }
