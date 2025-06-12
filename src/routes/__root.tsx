1b15bd2a6724898527c5cd025f8086b80f6ca946
Henry Agu <aguhenrychuks@gmail.com>
2025-06-12 13:42:10 +0100

    feat: routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import React from "react";
+import "../index.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
+import { Link } from "@tanstack/react-router"
+
 export const Route = createFileRoute({
-  component: RouteComponent,
+  component: HomeComponent,
 })
 
-function RouteComponent() {
-  return <div>This is the Home Page.</div>
+function HomeComponent() {
+  return <div className="flex items-center h-screen justify-center w-full">
+    <Link to="/resumeForm" className="bg-black text-white text-base px-5 py-2.5 rounded-sm transition-all duration-200 ease-in-out hover:opacity-75">Get started</Link>
+  </div>
 }
