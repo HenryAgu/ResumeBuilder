02ae5f7e452fe8c2f4bf17651985f6b87ef19036
Henry Agu <aguhenrychuks@gmail.com>
2025-06-26 15:19:42 +0100

    fix: education

--- src/components/modern template/education.tsx
+++ src/components/modern template/education.tsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
 
 interface Schools {
-  title: string;
+  name: string;
+  department: string;
 }
 
 const Education = () => {
   // schools array
   const schools: Schools[] = [
     {
-      title: "Bellows College | Madison, WI",
+      name: "Bellows College | Madison, WI",
+      department: "Bachelors Science in Nursing",
     },
     {
-      title: "Bachelors of Science in Nursing",
+      name: "Bachelors of Science in Nursing",
+      department: "Bachelors Science in Nursing",
     },
   ];
 
-  const [coursework, setCoursework] = useState<string>(
-    "Anatomy and physiology, pharmacology, nursing ethics, and patient care management."
-  );
+  const coursework: string =
+    "Anatomy and physiology, pharmacology, nursing ethics, and patient care management.";
   return (
     <section className="my-5">
       <p className="font-serif text-2xl font-medium">EDUCATION</p>
       <div className="my-2 mt-5 flex flex-col gap-y-2">
         {schools.map((school) => (
-          <p className="text-sm font-inter font-normal">{school.title}</p>
+          <div className="">
+            <p className="text-sm font-inter font-normal">{school.name}</p>
+            <p className="text-sm font-inter font-normal">
+              {school.department}
+            </p>
+          </div>
         ))}
         {/* Relevent works */}
         <p className="text-sm font-inter font-normal">
